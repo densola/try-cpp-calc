4c12002bce75e942404c487b390e9e1159f5fb0d
densola <107649045+densola@users.noreply.github.com>
2024-11-16 20:10:05 -0600

    Revise and remove comments

--- main.cc
+++ main.cc
@@ -4,12 +4,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // utils.cc
 
-void panic(std::string msg)
-{
-    std::cout << msg << std::endl;
-    exit(1);
-}
-
 bool isOperator(std::string s)
 {
     return (s == "+") || (s == "-") || (s == "*") || (s == "/");
@@ -17,11 +11,6 @@ bool isOperator(std::string s)
 
 bool isNumber(std::string s)
 {
-    // Condition looks out for the chance that s is the last
-    // character in the expression
-    if (s == ")")
-        return false;
-
     return std::stoi(s) * 0 == 0;
 }
 
@@ -45,7 +34,8 @@ int splitstringToArray(const std::string s, const char dl, const int l, std::str
             element += s[i];
     }
 
-    // handle the last element in s, which has no space after it.
+    // TODO
+    // handle the last element in s, which has/should have no space after it.
     dst[index] = element;
 
     return 0;
@@ -59,25 +49,24 @@ int splitstringToArray(const std::string s, const char dl, const int l, std::str
 int evaluateExpression(const std::string expr)
 {
     // Determine length for array.
-    // Start at 1 since last char will not be space.
     int len = 1;
-    for (char c : expr)
+    if (expr[expr.size() - 1] == ' ')
+        len--; // we shouldn't account for the last space
+
+    for (int i = 0; i < expr.size(); i++)
     {
-        if (c == ' ')
+        if (expr[i] == ' ')
             len++;
     }
+
     std::string exprValues[len];
 
     splitstringToArray(expr, ' ', len, exprValues);
 
-    // for (size_t i = 0; i < len; i++)
-    // {
-    //     std::cout << "exprVal:\t" << exprValues[i] << std::endl;
-    // }
-
+    // Expression evaluation with for loop
     std::string stack[len];
     int stackIndex = 0;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++) // TODO - Please improve this process lol
     {
         if (exprValues[i] == "(")
         {
@@ -85,10 +74,8 @@ int evaluateExpression(const std::string expr)
             continue;
         }
 
-        else if ((isOperator(exprValues[i]) || isNumber(exprValues[i])) && (i != (len - 1)))
+        else if ((exprValues[i] != ")") && (isOperator(exprValues[i]) || isNumber(exprValues[i])) && (i != (len - 1)))
         {
-            // std::cout << "isO|N\t" << exprValues[i] << std::endl;
-
             stack[stackIndex] = exprValues[i];
             stackIndex++;
 
@@ -97,15 +84,11 @@ int evaluateExpression(const std::string expr)
 
         else
         {
-            // std::cout << "clopa\t" << exprValues[i] << std::endl;
-
             if (i == (len - 1)) // handle the the last value.
             {
                 stack[stackIndex] = exprValues[i];
                 stackIndex++;
             }
-
-            std::cout << "\t\t\tstack[0] is " << stack[0] << std::endl;
 
             int tempAns = 0;
             while (stack[1] != "" && stack[2] != "")
@@ -113,7 +96,6 @@ int evaluateExpression(const std::string expr)
                 int num1 = std::stoi(stack[0]);
                 std::string oprtr = stack[1];
                 int num2 = std::stoi(stack[2]);
-                std::cout << "\t\t\t\tprocessing expression " << num1 << oprtr << num2 << std::endl;
 
                 if (oprtr == "+")
                 {
@@ -140,27 +122,14 @@ int evaluateExpression(const std::string expr)
                 int stackShiftIndex = 0;
                 while (stackShiftIndex < (len - 3))
                 {
-                    std::cout << std::endl
-                              << "\t\t\t\t\tstackShiftIndex == " << stackShiftIndex << std::endl;
-                    std::cout << "\t\t\t\t\treplacing " << stack[stackShiftIndex + 1] << " with " << stack[stackShiftIndex + 3] << std::endl;
-
                     stack[stackShiftIndex + 1] = stack[stackShiftIndex + 3];
                     stack[stackShiftIndex + 3] = ""; // clearing values to prevent infinite loop
                     stackShiftIndex++;
                 }
 
-                std::cout << "\t\t\t\tchanging " << stack[0] << " to " << tempAns << std::endl;
-
                 stack[0] = std::to_string(tempAns);
                 stackIndex = 1;
             }
-
-            std::cout << "\t\t\tstack is now: " << std::endl;
-            for (int k = 0; k < len; k++)
-            {
-                std::cout << "\t\t\t\tk == " << k << "\t" << "stack[k] == " << stack[k] << std::endl;
-            }
-            std::cout << std::endl;
 
             continue;
         }
